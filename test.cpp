9eb3e560235cd8854995c5dab3f237bfea129918
Wiktor Pierozak <pierozakwiktor@gmail.com>
2024-07-15 17:05:56 +0200

    Corrected main

--- test.cpp
+++ test.cpp
@@ -2,13 +2,15 @@
 #include "IPbusInterface_NO_QT.h"
 
 int main() {
-    boost::asio::io_service io;
-    IPbusTarget target(io);
-    target.reconnect();
-    for(int i = 0; i < 1000; i++)
-    {
-        std::cout<<"Wait...\n";
-        sleep(1);
-    }
+try {
+        boost::asio::io_service io_service;
+        IPbusTarget target(io_service);
+        
+        target.reconnect();
+
+        io_service.run();  // Run the io_service to process async operations
+    } catch (const std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    };
     return 0;
 }
