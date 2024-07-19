9915d52379ef3c4594a72a9f437af6f8f3bdcade
ALICE FIT AGH LAB <agh.alice.fit@hmail.com>
2024-07-19 22:39:59 +0200

    Mulit op probabli works

--- src/SWTelectronics.cpp
+++ src/SWTelectronics.cpp
@@ -10,40 +10,41 @@ void SWTelectronics::process_request(const char* swt_sequence)
 {
     std::string swt_str = swt_sequence;
     std::vector<std::string> lines = Utils::splitString(swt_str, "\n");
-    std::vector<std::pair<SWT, SWT_IPBUS_READY>> swt_frames;
+    std::vector<SWT> swt_frames;
 
     lines.erase(lines.begin());
-    lines.pop_back();
 
     for(auto frame : lines)
     {
-        std::cerr << frame << std::endl;
+        if(frame == "read") continue;
 
         frame = frame.substr(frame.find("0x")+2);
         int size = frame.size();
         for(int i = frame.find(','); i < size; i++)
             frame.pop_back();
-        
-        std::cerr << frame << std::endl;
 
-        SWT swt = string_to_swt(frame.c_str());
-        swt_frames.emplace_back(swt, swt_ready(swt));
-
-        m_packet.addTransaction(
-            (swt_frames.back().second.type == SWT_IPBUS_READY::Type::Read) ? TransactionType::data_read : TransactionType::data_write,
-             swt_frames.back().second.address, &swt_frames.back().second.data, 1);
+        swt_frames.emplace_back(string_to_swt(frame.c_str()));
+        TransactionType type = (swt_frames.back().getTransactionType() == SWT::TransactionType::READ) ? data_read : data_write;
+        m_packet.addTransaction(type, swt_frames.back().address, &swt_frames.back().data, 1);
     }
 
     if(transcieve(m_packet))
     {
-        for(int i = 0; i < swt_frames.size(); i++)
+        m_response = "success ";
+        for(int i = 0; i < lines.size(); i++)
         {
-            write_response(swt_frames[i].first, swt_frames[i].second);
-            if( i != swt_frames.size() - 1)
+            if(lines[i] == "read")
             {
+                write_response(swt_frames[i-1]);
                 m_response += "\n";
+                continue;
+            }
+            else if(lines[i].find("write") != std::string::npos)
+            {
+                m_response += "0\n";
             }
         }
+        setData(m_response.c_str());
     }
     else
     {
@@ -53,7 +54,7 @@ void SWTelectronics::process_request(const char* swt_sequence)
 
 void SWTelectronics::write_response(SWT frame)
 {
-    m_response = "success 0\n0x";
+    m_response += "0x";
     half_word h; 
     h.data = frame.mode;
 
@@ -66,6 +67,4 @@ void SWTelectronics::write_response(SWT frame)
     m_response += word_to_string(w);
     w.data = frame.data;
     m_response += word_to_string(w);
-    
-    setData(m_response.c_str());
 }
