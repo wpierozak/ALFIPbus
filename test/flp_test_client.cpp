46113aa6c27a8f6735f80412ef0e59f54d4f5ff8
ALICE FIT AGH LAB <agh.alice.fit@hmail.com>
2024-07-19 17:53:14 +0200

    Works

--- test/flp_test_client.cpp
+++ test/flp_test_client.cpp
@@ -6,9 +6,11 @@
 int main(int argc, const char** argv)
 {
     char bad[] = "BAD";
-    DimInfo subscribe("TEST/READ/RpcOut", bad);
-    std::string cafe = "1004CAFE";
-    std::cout<< "Sending comand..." << DimClient::sendCommand("TEST/READ/RpcIn", argv[1]) << std::endl;
+    DimInfo subscribe(argv[1], bad);
+    std::string cafe = argv[3];
+    cafe += ","; 
+    cafe += argv[4];
+    std::cout<< "Sending comand..." << DimClient::sendCommand(argv[2], cafe.c_str()) << std::endl;
     for(int i = 0; i < 5; i++)
     {
         std::this_thread::sleep_for(std::chrono::seconds(1));
--- test/flp_test_server.cpp
+++ test/flp_test_server.cpp
@@ -11,10 +11,10 @@ int main(int argc, const char** argv)
     SWTelectronics target(argv[2], io_service);
     target.debug_mode(IPbusTarget::DebugMode::Full);
     target.timer_tick(std::chrono::seconds(1));
-    //target.start_timer();
+    target.start_timer();
 
     DimServer::start(argv[1]);
-    for(int i = 0; i < 30; i++)
+    for(int i = 0; i < std::stoi(argv[3]); i++)
     {
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
