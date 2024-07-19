ab5c150a2ff106bc4f1bf4205aff0507eb5d2fc4
ALICE FIT AGH LAB <agh.alice.fit@hmail.com>
2024-07-19 17:31:08 +0200

    Adjusted to FRED

--- src/SWTelectronics.cpp
+++ src/SWTelectronics.cpp
@@ -1,5 +1,5 @@
 #include"SWTelectronics.h"
-
+#include<string>
 void SWTelectronics::rpcHandler() 
 {
     process_request(getString());
@@ -7,7 +7,16 @@ void SWTelectronics::rpcHandler()
 
 void SWTelectronics::process_request(const char* swt_sequence)
 {
-    std::string message = "100000000000"; message += swt_sequence;
+    std::string message = swt_sequence;
+    std::cerr << message << std::endl;
+    message = message.substr(message.find("0x")+2);
+    int size = message.size();
+    for(int i = message.find(','); i < size; i++)
+        message.pop_back();
+
+    message = "300000000000" + message;
+    std::cout<< "swt message: " << message << std::endl;
+
     SWT frame = string_to_swt(message.c_str());
     SWT_IPBUS_READY rframe = swt_ready(frame);
     m_packet.addTransaction((rframe.type == SWT_IPBUS_READY::Type::Read) ? TransactionType::ipread : TransactionType::ipwrite,
@@ -16,6 +25,10 @@ void SWTelectronics::process_request(const char* swt_sequence)
     if(transcieve(m_packet))
     {
         write_response(frame, rframe);
+    }
+    else
+    {
+        setData("failure");
     }
 }
 
@@ -26,14 +39,14 @@ void SWTelectronics::write_response(SWT frame, SWT_IPBUS_READY rframe)
     uint8_t buffer[10];
     swt_to_byte(frame, buffer);
 
-    m_response = "";
+    m_response = "success 0\n0x00000000000";
 
     for(int i = 3; i >= 0; i--)
     {
         m_response += hexToChar(buffer[i] >> 4);
         m_response += hexToChar(buffer[i] & 0x0F);
     }
-
-    std::cout << m_response << std::endl;
+    
+    //m_response += "\n";
     setData(m_response.c_str());
 }
