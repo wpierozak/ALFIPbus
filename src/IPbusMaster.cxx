89ab3985227ba0940bbdc8cc83301d6792167631
VictorPierozak <agh.alice.fit@gmail.com>
2024-08-05 13:13:20 +0200

    idx_request RMWsum fixed

--- src/IPbusMaster.cxx
+++ src/IPbusMaster.cxx
@@ -289,7 +289,7 @@ bool IPbusMaster::processResponse(IPbusRequest& request, IPbusResponse& response
           if (request.getDataOut(idx) != nullptr) {
             memcpy(request.getDataOut(idx), response.getBuffer() + idx_response, wordSize);
             idx_response++;
-            idx_request += headerResponse->words;
+            idx_request += 2;
           }
         } break;
 
