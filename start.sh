0597b3f7bce0ffca0b5eccfd68227b9564a13664
ALICE FIT AGH LAB <agh.alice.fit@gmail.com>
2024-08-22 20:14:57 +0200

    Fixed growing response time

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+log_filename="log/logs"
+current_time=$(date +%Y%m%d%H%M%S);
+log_filename="${log_filename}_${current_time}.txt"
 bin/AlfIPbus \
             -n ALF_FTM \
             -l 172.20.75.175:50001 \
-            -t 1000 
+            -t 1000 \
+            -f $log_filename
