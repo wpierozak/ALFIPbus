c283937e202eb2113cfb92e83da2a162cf1909aa
ALICE FIT AGH LAB <agh.alice.fit@gmail.com>
2024-08-27 23:01:21 +0200

    Changed logs

--- src/SwtLink.cxx
+++ src/SwtLink.cxx
@@ -173,7 +173,7 @@ bool SwtLink::interpretFrames()
 
 void SwtLink::sendResponse()
 {
-  BOOST_LOG_TRIVIAL(debug) << "Request successfuly processed - sending response";
+  BOOST_LOG_TRIVIAL(info) << "Request successfuly processed - sending response";
   m_fredResponse = "success\n" + m_fredResponse;
   setData(m_fredResponse.c_str());
 }
