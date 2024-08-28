6c0456de54bdf26ef971399c4612e7c54987137b
ALICE FIT AGH LAB <agh.alice.fit@gmail.com>
2024-08-28 15:56:38 +0200

    Update start_mock.sh

--- start_mock.sh
+++ start_mock.sh
@@ -3,7 +3,7 @@ log_filename="log/logs_mock"
 current_time=$(date +%Y%m%d%H%M%S);
 log_filename="${log_filename}_${current_time}.txt"
 bin/AlfIPbus \
-            -n ALF_FTM \
+            -n ALF_MOCK \
             -l 127.0.0.1:50001 \
             -t 1000 \
 #            -f $log_filename
