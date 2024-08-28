19d56e3f0a1007f33e64ad1e79f4b09738fe0e6f
ALICE FIT AGH LAB <agh.alice.fit@gmail.com>
2024-08-28 14:26:10 +0200

    Update log formatting with colors

--- inc/logging.h
+++ inc/logging.h
@@ -16,33 +16,68 @@ namespace logging
 namespace bl = boost::log;
 namespace ble = boost::log::expressions;
 
-void init(std::string filename = "")
+void coloring_formatter(const boost::log::record_view& record, boost::log::formatting_ostream& stream) {
+  auto severity = record[boost::log::trivial::severity];
+  assert(severity);
+
+  stream << "[" << bl::extract<boost::posix_time::ptime>("TimeStamp", record) << "] [";
+
+  switch (severity.get()) {
+    case bl::trivial::severity_level::trace:
+      stream << "\e[35m";
+      break;
+    case bl::trivial::severity_level::debug:
+      stream << "\e[34m";
+      break;
+    case bl::trivial::severity_level::info:
+      stream << "\e[32m";
+      break;
+    case bl::trivial::severity_level::warning:
+      stream << "\e[93m";
+      break;
+    case bl::trivial::severity_level::error:
+      stream << "\e[91m";
+      break;
+    case bl::trivial::severity_level::fatal:
+      stream << "\e[41m";
+      break;
+  }
+
+  stream << severity;
+
+  if (severity) {
+    stream << "\033[0m";
+  }
+
+  stream << "] " << record[boost::log::expressions::message];
+}
+
+void non_coloring_formatter(const boost::log::record_view& record, boost::log::formatting_ostream& stream) {
+  auto severity = record[boost::log::trivial::severity];
+  assert(severity);
+
+  stream << "[" << bl::extract<boost::posix_time::ptime>("TimeStamp", record) << "] [";
+
+  stream << severity;
+
+  stream << "] " << record[boost::log::expressions::message];
+}
+
+void init(std::string filename = "", bool verbose = false)
 {
   bl::add_common_attributes();
 
   if (filename == "") {
-    bl::add_console_log(std::cout,
-                        bl::keywords::format = (ble::stream
-                                                << "["
-                                                << ble::format_date_time<boost::posix_time::ptime>("TimeStamp", "%Y-%m-%d %H:%M:%S.%f")
-                                                << "] "
-                                                << "[" << std::setw(7) << bl::trivial::severity << "]:\t"
-                                                << ble::smessage));
+    auto consoleLog = bl::add_console_log(std::cout);
+    consoleLog->set_formatter(&coloring_formatter);
   } else {
-    bl::add_file_log(bl::keywords::file_name = filename,
-                     bl::keywords::auto_flush = true,
-                     bl::keywords::format = (ble::stream
-                                             << "["
-                                             << ble::format_date_time<boost::posix_time::ptime>("TimeStamp", "%Y-%m-%d %H:%M:%S.%f")
-                                             << "] "
-                                             << "[" << std::setw(7) << bl::trivial::severity << "]:\t"
-                                             << ble::smessage));
+    auto fileLog = bl::add_file_log(bl::keywords::file_name = filename, bl::keywords::auto_flush = true);
+    fileLog->set_formatter(&non_coloring_formatter);
   }
 
-#ifndef DEBUG
-  bl::core::get()->set_filter(
-    bl::trivial::severity >= bl::trivial::info);
-#endif
+  if(!verbose)
+    bl::core::get()->set_filter(bl::trivial::severity >= bl::trivial::info);
+
   BOOST_LOG_TRIVIAL(debug) << "Debug log mode enabled";
 }
 } // namespace logging
---
+++ start_mock.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+log_filename="log/logs_mock"
+current_time=$(date +%Y%m%d%H%M%S);
+log_filename="${log_filename}_${current_time}.txt"
+bin/AlfIPbus \
+            -n ALF_FTM \
+            -l 127.0.0.1:50001 \
+            -t 1000 \
+#            -f $log_filename
